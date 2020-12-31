6c9c1e7be642207598f7f3672801e52e41d50354
Matias Insaurralde <matias@insaurral.de>
2020-12-31 20:14:35 -0300

    Improve labels and map styling

--- js/fetch.js
+++ js/fetch.js
@@ -2,6 +2,15 @@ const ENDPOINT_AQI = "https://rald-dev.greenbeep.com/api/v1/aqi";
 const DEFAULT_CENTER = [-25.250, -57.536]
 const DEFAULT_ZOOM = 11
 
+const CAT_LABEL_STYLE = {
+  'Good': {label: 'Libre', class: 'reading-info--is-good'},
+  'Moderate': {label: 'Maso', class: 'reading-info--is-moderate'},
+  'Unhealthy for Sensitive Groups': {label: 'No tan bien', class: 'reading-info--is-sensitive'},
+  'Unhealthy': {label: 'Insalubre', class: 'reading-info--is-unhealthy'},
+  'Very Unhealthy': {label: 'Muy insalubre', class: 'reading-info--is-very-unhealthy'},
+  'Hazardous': {label: 'Peligroso', class: 'reading-info--is-dangerous'}
+}
+
 function requestGeoData() {
     var map = this.map;
     var searchUrl = ENDPOINT_AQI;
@@ -27,12 +36,14 @@ function requestGeoData() {
 function loadMarkers(map, data) {
     var layerGroup = L.geoJSON(data, {
         onEachFeature: function (feature, layer) {
+            var cat = CAT_LABEL_STYLE[feature.properties.quality.category];
             var popup =
                 '<h1>' + feature.properties.description +
                 '</h1><span class="has-text-weight-bold">AQI: ' +
                 feature.properties.quality.index + '</span>' +
-                '<br>' + feature.properties.quality.category
-            layer.bindPopup(popup, {autoClose: false});
+                '<br>' + cat.label
+            layer.bindPopup(popup, {autoClose: false, className: cat.class});
+            console.log(cat)
         },
     }).addTo(map);
 
