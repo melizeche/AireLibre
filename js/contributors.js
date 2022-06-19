2eb21a72cfe9a809138b46e43caa5d5912848fc7
Lucas Ginard <lucasmatiasginard@hotmail.es>
2022-06-18 21:39:45 -0400

    implement contributors page

---
+++ js/contributors.js
@@ -0,0 +1,47 @@
+const getContributors = () => {
+    Promise.all([
+        fetch(`https://api.github.com/repos/melizeche/AireLibre/contributors`), 
+        fetch(`https://api.github.com/repos/tchx84/linka/contributors`),
+        fetch(`https://api.github.com/repos/garyservin/linka-firmware/contributors`),
+        fetch(`https://api.github.com/repos/melizeche/linkaBot/contributors`),
+        fetch(`https://api.github.com/repos/matiasinsaurralde/aqmap/contributors`),
+        fetch(`https://api.github.com/repos/LucasGinard/AireLibre-Android/contributors`),
+      ])
+        .then(async([aireLibre, linka,linkaFirmware,linkBot,aqmap,appAndroid]) => {
+            let contributors = [];
+            contributors.push(await aireLibre.json());
+            contributors.push(await linka.json());
+            contributors.push(await linkaFirmware.json());
+            contributors.push(await linkBot.json());
+            contributors.push(await aqmap.json());
+            contributors.push(await appAndroid.json());
+            let listContributors = [];
+            contributors.forEach(element => {
+                element.forEach(auxElement =>{
+                    listContributors.push(auxElement);
+                });
+            });
+            listContributors = listContributors.filter((thing, index, self) =>
+                index === self.findIndex((t) => (
+                t.login === thing.login
+                ))
+            );
+            console.log(listContributors)
+            const div = document.querySelector(".contributorsContainer");
+            div.innerHTML = listContributors.map(u => 
+                (`<div class="divContributor">
+                <a class="link" href="${u.html_url}" target="_blank">
+                <img id="contributorsImg" src="${u.avatar_url}" 
+                    alt="imgContruibutor"  
+                />
+                </a>
+                <p class=colorTheme>${u.login}</p>
+                </div>`
+                )
+                ).join('<br />'); 
+
+            getSaveTheme();
+        })
+        .catch((error) => console.error(error));
+};
+getContributors();
